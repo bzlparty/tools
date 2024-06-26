692beb62446407b11c552dd105163032eab9d396
Marc Redemske <code@mgred.dev>
2024-06-26 14:34:30 +0200

    refactor(release): optimize build

--- scripts/release/build.sh
+++ scripts/release/build.sh
@@ -6,6 +6,5 @@ export TAG=${GITHUB_REF_NAME:-main}
 rm -rf "$DEST"
 mkdir "$DEST"
 
-bazel build //cmd/...
 bazel run --action_env=TAG="$TAG" //scripts/release:copy_assets
 bazel run --action_env=TAG="$TAG" //scripts/release:git_archive
