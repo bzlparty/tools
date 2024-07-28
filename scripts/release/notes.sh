a3484afe8b64968e8269c0366c43f9caae4f3222
Marc Redemske <code@mgred.dev>
2024-07-28 19:58:31 +0200

    fix(release): set `TAG` var

--- scripts/release/notes.sh
+++ scripts/release/notes.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 DEST="$(pwd)/dist"
+TAG=${GITHUB_REF_NAME:-main}
 
 export DEST
 
 bazel run \
   --@bzlparty_tools//lib:release_dir="$DEST" \
-  --@bzlparty_tools//lib:release_tag="$GITHUB_REF_NAME" \
+  --@bzlparty_tools//lib:release_tag="$TAG" \
   //scripts/release:release_notes
