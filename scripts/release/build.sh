b66f067d0b6a6473eaac0378831a7c59be6af04e
Marc Redemske <code@mgred.dev>
2024-07-28 19:42:48 +0200

    refactor(release): easy release

--- scripts/release/build.sh
+++ scripts/release/build.sh
@@ -2,6 +2,7 @@
 
 DEST="$(pwd)/dist"
 URL="https://github.com/bzlparty/tools/releases/download/$TAG"
+TAG=${GITHUB_REF_NAME:-main}
 
 if [ -z "$CI" ]; then
   URL="file://$DEST"
@@ -14,6 +15,6 @@ export DEST
 bazel run //scripts/release:copy_assets
 bazel run \
   --@bzlparty_tools//lib:release_dir="$DEST" \
-  --@bzlparty_tools//lib:release_tag="$GITHUB_REF_NAME" \
+  --@bzlparty_tools//lib:release_tag="$TAG" \
   --//scripts/release:assets_url="$URL" \
   //scripts/release:git_archive
